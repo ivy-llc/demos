bd7a80a223a293a568a6176c0cd5b75d810775e8
Aarsh2001 <ac5973@srmist.edu.in>
2023-08-09 19:31:52 +0530

    resolved a few more issues

--- tests/test_demos.sh
+++ tests/test_demos.sh
@@ -9,7 +9,7 @@ touch .ivy/key.pem
 echo -n "$1"> .ivy/key.pem
 
 # install dependencies
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt >/dev/null 2>&1
 
 # run test
 python3 tests/notebook_testing.py
