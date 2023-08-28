d99839c68e362bd46527791a808a795331fe8ae6
Kareem Morsy <kreem.morsy@hotmail.com>
2023-08-28 19:30:37 +0000

    feat: Migrated to sphinx base

---
+++ docs/partial_conf.py
@@ -0,0 +1,6 @@
+# Retrieve html_theme_options from docs/conf.py
+from docs.conf import html_theme_options
+
+html_theme_options["switcher"]["json_url"] = "https://unify.ai/docs/versions/ivy.json"
+
+nbsphinx_execute = 'never'
