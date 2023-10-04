ebcbf668d415e55586f21c16a2071f65ed562d94
Aarsh2001 <ac5973@srmist.edu.in>
2023-10-04 20:03:31 +0100

    gcp auth

--- .github/auth/vm_auth.py
+++ .github/auth/vm_auth.py
@@ -40,7 +40,7 @@ def start_runner(creds, key, ssh_username, id = "gpu-insatnce", zone='us-central
     return output
 
 if __name__ == "__main__":
-    key, username = sys.argv[1], sys.argv[2]
+    username, key = sys.argv[1], sys.argv[2]
     # Start the instance
     start_runner('gcp_auth.json', str(key), str(username))
 
