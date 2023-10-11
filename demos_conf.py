5271e036fdec5e2f5212499621bdaf55e3e6d7d2
vedpatwardhan <vedpat3@gmail.com>
2023-10-11 21:43:30 +0530

    refactor: renamed compile to trace in all the demos except wip

--- demos_conf.py
+++ demos_conf.py
@@ -2,7 +2,7 @@ youtube_map = {
     "demos/quickstart": "9myf5tekBWU",
     "demos/learn_the_basics/01_write_ivy_code": "lYW_V4ZkYEo",
     "demos/learn_the_basics/02_unify_code": "YKrxYB-1Xio",
-    "demos/learn_the_basics/03_compile_code": "fqnhEi-qA4w",
+    "demos/learn_the_basics/03_trace_code": "fqnhEi-qA4w",
     "demos/learn_the_basics/04_transpile_code": "6WCYOF1vdLw",
     "demos/learn_the_basics/05_lazy_vs_eager": "xav4HYfvPOU",
     "demos/learn_the_basics/06_how_to_use_decorators": "fueduIEMkbY",
