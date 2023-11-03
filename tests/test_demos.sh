6cd30142fbb8e4399d98429bd8272c1fc5e921bc
Ved Patwardhan <54766411+vedpatwardhan@users.noreply.github.com>
2023-11-03 09:20:13 +0300

    another minor change

--- tests/test_demos.sh
+++ tests/test_demos.sh
@@ -2,6 +2,7 @@
 
 cd ivy
 python3 -m pip install .
+conda install -c "nvidia/label/cuda-11.8.0" cuda-nvcc
 
 cd ..
 cd demos
@@ -15,4 +16,4 @@ python3 -m pip install -r requirements.txt >/dev/null 2>&1
 
 # run test
 echo "$2"
-python3 tests/notebook_testing.py "$2" "$3"
+python3 new_tests/main.py "$2"
